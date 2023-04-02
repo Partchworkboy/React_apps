0094d2594c5aee309ec5b6fc9eebb96283415298
Fred <59137832+Partchworkboy@users.noreply.github.com>
2023-04-02 16:58:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import "./styles.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Calendar from './components/Calendar';
+import Todo from './components/Todo';
+import TodoList from './components/TodoList';
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Brea's Planner App</h1>
+      </header>
+      <main>
+        <h2>Calendar </h2>
+          <div className="calender-container">
+                 <Calendar />
+          </div>
+        <h2>To Do List</h2>
+          <div className="todo-list">
+                  <Todo />
+          </div>
+      </main>
+      <footer>
+        <p>&copy; 2023 Brea's Planner App</p>
+      </footer>
     </div>
   );
 }
+
+export default App;
